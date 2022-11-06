13de86d39d42693e173619f138d7ca1e3263d042
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-06 16:25:37 -0500

    adjusted deploy script

---
+++ .github/scripts/npm/can-deploy.js
@@ -0,0 +1,11 @@
+const { execSync } = require('child_process');
+const chalk = require('chalk');
+
+if (execSync('git rev-parse --abbrev-ref HEAD').toString('utf8').trim() === 'main') {
+    process.exit(0);
+} else {
+    console.log(chalk.red(`You can only deploy from the ${chalk.white.underline('main')} branch!`));
+    console.log(` - Deployment updates the version of the project so it can publish to NPM`);
+    console.log(` - This can only be done on the main branch to avoid merge conflicts`);
+    process.exit(1);
+}
