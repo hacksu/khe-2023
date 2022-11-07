e3710a07aba707b6c7525eb3afe31bf925024b97
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-07 00:55:39 -0500

    adjusted mongodb connection log

--- projects/server/src/utils/mongo/index.ts
+++ projects/server/src/utils/mongo/index.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import { config } from '../../config';
+import { log } from '../logging';
+import chalk from 'chalk';
 
 mongoose.connect(config.mongo).then(() => {
-    console.log(`connected to ${config.mongo}`);
+    log.info(`Connected to ${chalk.green('MongoDB')}`, 'with', chalk.grey(config.mongo));
 });
 
