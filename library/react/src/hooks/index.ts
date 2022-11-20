58c9998f5a7a39556f451b96efbdbea95b31bd44
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-20 15:27:16 -0500

    migrated stuff to react lib

--- library/react/src/hooks/index.ts
+++ library/react/src/hooks/index.ts
@@ -7,3 +7,13 @@ export {
     useForm,
     useMediaQuery
 }
+
+
+export function mappedActions<Actions extends Record<string, (...args: any[]) => any>>(actions: Actions) {
+    return function <
+        Key extends keyof Actions,
+        Action extends ((...args: any[]) => any) = Actions[Key]
+    >(action: Key, ...args: Parameters<Action>): ReturnType<Action> {
+        return actions[action](...args);
+    }
+}
