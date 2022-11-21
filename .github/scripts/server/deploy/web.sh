94d3e3c31cbeebb112eb249a605b2ddf2b2a1e40
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-21 16:33:45 -0500

    adjusted

---
+++ .github/scripts/server/deploy/web.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Environment Variables
+CWD=$( pwd );
+REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && git rev-parse --show-toplevel );
+PROJECT=web
+
+echo "building $PROJECT"
+BUILD_DIR=.next-build npm run build -- --only --filter=@kenthackenough/staff
+
+cd $REPO/projects/$PROJECT
+
+# Stop the project
+if pm2 show $PROJECT; then
+    pm2 stop $PROJECT
+fi
+
+# Erase old build folder
+rm -rf .next
+
+# Copy over the new one
+if mv .next-build .next; then
+    cd $REPO
+#   Start the project
+    if pm2 show $PROJECT; then
+        pm2 start $PROJECT
+    else
+        pm2 start --name $PROJECT "npm run start -- --only --filter=@kenthackenough/$PROJECT"
+    fi
+    pm2 show $PROJECT
+fi
