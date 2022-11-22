675e28cdd1081d8ef58660b5c0c12b7b86ddcdf5
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-22 17:18:01 -0500

    fixed intentional error

--- projects/staff/src/pages/index.tsx
+++ projects/staff/src/pages/index.tsx
@@ -2,9 +2,10 @@ import { Box, Progress } from '@mantine/core';
 import { useState } from 'react';
 import { api } from '../utils/trpc';
 import { TicketsList } from '../widgets/tickets/list';
-import bruh from 'this-should-error';
 
-bruh();
+
+// import bruh from 'this-should-error';
+// bruh();
 
 export default function Homepage() {
     const ping = api.ping.useQuery();
