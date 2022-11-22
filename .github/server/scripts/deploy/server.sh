64cc632379d5f528e4da9ca30f26d55fae59bcba
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-21 20:44:29 -0500

    wip cool deployment stuff

--- .github/server/scripts/deploy/server.sh
+++ .github/server/scripts/deploy/server.sh
@@ -26,6 +26,26 @@ if mv dist-build dist; then
     fi
     pm2 show $PROJECT
     rm -rf dist-old
+
+#   NGINX
+    mkdir -p /etc/nginx/includes
+    cd /etc/nginx
+    mv includes old-includes
+    mv sites-enabled old-sites-enabled
+    cp -R $REPO/.github/server/nginx/includes /etc/nginx/includes
+    cp -R $REPO/.github/server/nginx/sites-enabled /etc/nginx/sites-enabled
+    if nginx -t; then
+        rm -rf /etc/nginx/old-includes
+        rm -rf /etc/nginx/old-sites-enabled
+        service nginx reload
+    else
+        nginx -t
+        rm -rf /etc/nginx/includes
+        rm -rf /etc/nginx/sites-enabled
+        mv old-includes includes
+        mv old-sites-enabled sites-enabled
+        exit 1
+    fi
 else
     mv dist-old dist
     exit 1
