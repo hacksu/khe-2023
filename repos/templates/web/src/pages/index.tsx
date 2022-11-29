11bf303e2e4b1b0d4fde31b62a851bcfcec5a157
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-29 02:13:03 -0500

    a

--- repos/templates/web/src/pages/index.tsx
+++ repos/templates/web/src/pages/index.tsx
@@ -7,19 +7,11 @@ import { useState } from 'react';
 
 export default function Homepage() {
     const ping = api.ping.useQuery();
-    const [date, setDate] = useState<Date>(null!);
-    api.onDate.useSubscription(undefined, {
-        onData(data) {
-            setDate(data.date);
-        },
-    })
     return <div>
         <Title>Hey! Kent Hack Enough Website!</Title>
         <p>wooah its the web template project!</p>
         <p>ping: {ping?.data?.toLocaleString() || 'loading...'}</p>
-        <p>subscription date: {date?.toLocaleString() || 'loading...'}</p>
         <Link href={'/contact'}>contact us!</Link>
-        
     </div>
 }
 
