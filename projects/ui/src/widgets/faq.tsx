b61e68c2458801c1095a3254552e2ac53e60ba12
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-06 13:41:00 -0500

    wip faq

--- projects/ui/src/widgets/faq.tsx
+++ projects/ui/src/widgets/faq.tsx
@@ -1,18 +1,49 @@
 import { Accordion, AccordionProps } from '@mantine/core';
+import { ReactMarkdownRenderer } from '@kenthackenough/mdx';
+import { Button } from '@mantine/core';
 import { api } from '../utils/trpc';
+import { useEffect, useMemo, useState } from 'react';
+import type { FrequentlyAskedQuestion } from '@kenthackenough/server/data';
 
 /** @export 'faq' */
+
+
+const mdx = new ReactMarkdownRenderer({
+    components: {
+        Button,
+    },
+    styles: []
+})
+
+
+async function compileQuestions(questions: FrequentlyAskedQuestion[]) {
+    const renders = await Promise.all(questions.map(o => {
+        return mdx.compile(o.answer)
+    }));
+    return questions.map((o, i) => {
+        return Object.assign(o, {
+            component: renders[i]
+        })
+    });
+}
 
 export function useFAQ() {
     const query = api.content.faq.list.useQuery();
     const questions = query.data?.questions || [];
-    
+    const [compiled, setCompiled] = useState<(FrequentlyAskedQuestion & {
+        component?: JSX.Element | string
+    })[]>(questions.map(o => Object.assign(o, { component: o.answer })));
+    useEffect(() => {
+        compileQuestions(questions).then(setCompiled);
+    }, [questions]);
+    return compiled;
 }
 
 export function FrequentlyAskedQuestions(props: Omit<AccordionProps, 'children'>) {
-    const query = api.content.faq.list.useQuery();
+    // const query = api.content.faq.list.useQuery();
+    const questions = useFAQ();
     return <Accordion {...props}>
-        {(query.data?.questions || []).map(({ id, answer, question }) => <Accordion.Item value={id} key={id}>
+        {questions.map(({ id, answer, question }) => <Accordion.Item value={id} key={id}>
             <Accordion.Control>{question}</Accordion.Control>
             <Accordion.Panel>{answer}</Accordion.Panel>
         </Accordion.Item>)}
--- repos/templates/web/src/ui/components/faq.tsx
+++ repos/templates/web/src/ui/components/faq.tsx
@@ -1,13 +1,13 @@
 import { api } from '@kenthackenough/ui/trpc';
 import { Accordion, AccordionProps } from '@mantine/core';
+export { FrequentlyAskedQuestions } from '@kenthackenough/ui/faq';
 
-
-export function FrequentlyAskedQuestions(props: Omit<AccordionProps, 'children'>) {
-    const query = api.content.faq.list.useQuery();
-    return <Accordion {...props}>
-        {(query.data?.questions || []).map(({ id, answer, question }) => <Accordion.Item value={id} key={id}>
-            <Accordion.Control>{question}</Accordion.Control>
-            <Accordion.Panel>{answer}</Accordion.Panel>
-        </Accordion.Item>)}
-    </Accordion>
-}
+// export function FrequentlyAskedQuestions(props: Omit<AccordionProps, 'children'>) {
+//     const query = api.content.faq.list.useQuery();
+//     return <Accordion {...props}>
+//         {(query.data?.questions || []).map(({ id, answer, question }) => <Accordion.Item value={id} key={id}>
+//             <Accordion.Control>{question}</Accordion.Control>
+//             <Accordion.Panel>{answer}</Accordion.Panel>
+//         </Accordion.Item>)}
+//     </Accordion>
+// }
