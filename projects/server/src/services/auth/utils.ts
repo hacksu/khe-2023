a874477f33592b7fe0a52e5d68586329c37dc3e3
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-12 13:33:57 -0500

    wip

--- projects/server/src/services/auth/utils.ts
+++ projects/server/src/services/auth/utils.ts
@@ -1,5 +1,6 @@
 import { OAuthConfig } from 'next-auth/providers/index';
 import { authProviders } from './config';
+import { get, set } from 'lodash';
 
 
 type PROVIDER_PUBLIC_FIELDS = typeof PROVIDER_PUBLIC_FIELDS[number];
@@ -13,13 +14,25 @@ export type PublicAuthProviders = {
     [P in keyof typeof authProviders]: PublicAuthProvider<P>
 }
 
-export const publicAuthProviders: PublicAuthProviders = Object.fromEntries(
-    Object.entries(authProviders).map(([id, provider]) => ([
-        id,
-        Object.fromEntries(
-            Object.entries(provider).filter(o => PROVIDER_PUBLIC_FIELDS.includes(o[0] as any))
-        )
-    ]))
-) as any;
+// export const publicAuthProviders: PublicAuthProviders = Object.fromEntries(
+//     Object.entries(authProviders).map(([id, provider]) => ([
+//         id,
+//         Object.fromEntries(
+//             Object.entries(provider).filter(o => PROVIDER_PUBLIC_FIELDS.includes(o[0] as any))
+//         )
+//     ]))
+// ) as any;
 
+export const publicAuthProviders: PublicAuthProviders = Object.fromEntries(
+    Object.entries(authProviders).map(([id, provider]) => {
+        const data = Object.fromEntries(
+            Object.entries(provider).filter(o => PROVIDER_PUBLIC_FIELDS.includes(o[0] as any))
+        );
+        if (provider.type === 'oauth') {
+            set(data, 'style.logo', `https://authjs.dev/img/providers/` + get(data, 'style.logo'))
+            set(data, 'style.logoDark', `https://authjs.dev/img/providers/` + get(data, 'style.logoDark'))
+        }
+        return [id, data];
+    })
+) as any;
 
