864a71a91e0639be2327a12ac9176afc33a48178
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-12 21:30:45 -0500

    a

---
+++ projects/ui/src/components/auth/pages/login.tsx
@@ -0,0 +1,10 @@
+import { Box, Text } from '@mantine/core';
+
+/** @export 'auth/pages/login' */
+
+export function LoginPage() {
+    return <Box>
+        <Text>UI Login Page</Text>
+    </Box>
+}
+
---
+++ projects/ui/src/components/auth/pages/logout.tsx
@@ -0,0 +1,10 @@
+import { Box, Text } from '@mantine/core';
+
+/** @export 'auth/pages/logout' */
+
+export function LogoutPage() {
+    return <Box>
+        <Text>UI Logout Page</Text>
+    </Box>
+}
+
