f03510ece5efaa0d8c4b6f7ec3fc73f2b0d54046
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-23 18:53:01 -0500

    a

--- projects/staff/src/middleware.ts
+++ projects/staff/src/middleware.ts
@@ -1,11 +1,12 @@
+import { getSession } from '@kenthackenough/server/auth/session';
 import { NextResponse } from 'next/server'
 import type { NextMiddleware, NextRequest } from 'next/server'
 
 
 export const middleware: NextMiddleware = (req, event) => {
-    console.log('doing middleware', req);
+    // console.log('doing middleware', req);
     if (req.nextUrl.pathname.startsWith('/login')) {
-        
+        getSession({ req: req as any, res: {} as any }).then(o => console.log('got sess', o))
     }
     return NextResponse.next();
 }
